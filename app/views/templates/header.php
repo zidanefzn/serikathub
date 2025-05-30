33c7b77ad63f1d992225156a0ecbcc27aff313d5
zidanefzn <zidan4164@gmail.com>
2025-05-30 10:44:34 +0700

    add web icon

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>SerikatHub | <?= $data['judul']; ?></title>
+    <link rel="website icon" type="png" href="<?= BASEURL ?>/img/web-icon.png">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
